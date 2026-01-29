49f9aff17ac9bfcadbbd80b2b63495bf6013695f
Bozhidar Dryanovski <bdryanovski@users.noreply.github.com>
2026-01-29 23:05:43 +0200

    feat: clean some packages
    Signed-off-by: Bozhidar Dryanovski <bdryanovski@users.noreply.github.com>

--- lua/bdryanovski/plugins/incline.lua
+++ lua/bdryanovski/plugins/incline.lua
@@ -5,14 +5,13 @@ return {
 	priority = 1200,
 	config = function()
 		local devicons = require("nvim-web-devicons")
-		local navic = require("nvim-navic")
 		local helpers = require("incline.helpers")
 		require("incline").setup({
 			window = {
 				padding = 0,
 				margin = { horizontal = 0 },
 			},
-			hide = { cursorline = true },
+			hide = { cursorline = true, focused_win = true },
 			render = function(props)
 				local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
 				if filename == "" then
@@ -24,16 +23,6 @@ return {
 					ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
 					{ " " .. filename .. " ", gui = vim.bo[props.buf].modified and "bold,italic" or "bold" },
 				}
-
-				if props.focused then
-					for _, item in ipairs(navic.get_data(props.buf) or {}) do
-						table.insert(res, {
-							{ " > ", group = "NavicSeparator" },
-							{ item.icon, group = "NavicIcons" .. item.type },
-							{ item.name, group = "NavicText" },
-						})
-					end
-				end
 
 				table.insert(res, " ")
 
--- lua/bdryanovski/plugins/nvim-lspconfig.lua
+++ lua/bdryanovski/plugins/nvim-lspconfig.lua
@@ -4,31 +4,4 @@ return {
 	dependencies = {
 		"saghen/blink.cmp",
 	},
-	config = function()
-		-- Add a diagnostic command to see what's handling textDocument/diagnostic
-		vim.api.nvim_create_user_command("LspDiagHandler", function()
-			print("Checking handlers for textDocument/diagnostic...")
-			local clients = vim.lsp.get_active_clients()
-
-			for _, client in ipairs(clients) do
-				local supports_diagnostics = client.supports_method
-					and client.supports_method("textDocument/diagnostic")
-
-				print(
-					string.format(
-						"%s (id: %d): supports textDocument/diagnostic = %s",
-						client.name,
-						client.id,
-						tostring(supports_diagnostics or false)
-					)
-				)
-
-				if client.name == "eslint" or client.name:match("eslint") then
-					print("ESLint config details:")
-					print("  Root dir: " .. (client.config.root_dir or "N/A"))
-					vim.print(client.config.settings or {})
-				end
-			end
-		end, {})
-	end,
 }
