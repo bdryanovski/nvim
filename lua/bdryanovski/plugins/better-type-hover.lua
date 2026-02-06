438305853260f013054950fe984ee81384db02aa
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2026-02-06 23:38:44 +0200

    updte plugins
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- lua/bdryanovski/plugins/better-type-hover.lua
+++ lua/bdryanovski/plugins/better-type-hover.lua
@@ -3,10 +3,5 @@ return {
 	ft = { "typescript", "typescriptreact" },
 	config = function()
 		require("better-type-hover").setup()
-
-		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-			vim.lsp.handlers.hover,
-			{} -- e.g. { border = "rounded" }
-		)
 	end,
 }
