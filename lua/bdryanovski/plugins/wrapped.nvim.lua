3ecdfdd14e932ec9449f5696ff860de6abeca7c0
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2026-02-16 12:20:06 +0200

    feat: copy the default config from the wrapped.nvim package
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- lua/bdryanovski/plugins/wrapped.nvim.lua
+++ lua/bdryanovski/plugins/wrapped.nvim.lua
@@ -2,5 +2,21 @@ return {
 	"aikhe/wrapped.nvim",
 	dependencies = { "nvzone/volt" },
 	cmd = { "NvimWrapped" },
-	opts = {},
+	opts = {
+		path = vim.fn.stdpath("config"), -- path to your neovim configuration (defaults to nvim config)
+		border = false,
+		size = {
+			width = 120,
+			height = 40,
+		},
+		exclude_filetype = {
+			".gitmodules",
+		},
+		cap = {
+			commits = 1000,
+			plugins = 100,
+			plugins_ever = 200,
+			lines = 10000,
+		},
+	},
 }
