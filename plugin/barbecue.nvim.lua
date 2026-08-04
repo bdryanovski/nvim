d7952194f9182a820d2554ea6ec736c344a62f19
Bozhidar Dryanovski <bdryanovski@users.noreply.github.com>
2026-08-04 10:06:09 +0300

    fix: some junk
    Signed-off-by: Bozhidar Dryanovski <bdryanovski@users.noreply.github.com>

--- plugin/barbecue.nvim.lua
+++ plugin/barbecue.nvim.lua
@@ -44,9 +44,6 @@ vim.api.nvim_create_autocmd({
     'BufWinEnter',
     'CursorHold',
     'InsertLeave',
-
-    -- include this if you have set `show_modified` to `true`
-    'BufModifiedSet',
 }, {
     group = vim.api.nvim_create_augroup('barbecue.updater', {}),
     callback = function()
